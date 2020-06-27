b83effe749407e990174224a266757ef05ee1646
Cintia Zago <cintiazago@ivendas.com.br>
2020-06-27 14:59:23 -0300

    Implementando criacao de usuarios; criptografia de senha

--- level-02/gobarber/backend/src/routes/index.ts
+++ level-02/gobarber/backend/src/routes/index.ts
@@ -1,9 +1,12 @@
 // src/routes/index.ts
 import { Router } from 'express';
+
 import appointmentsRouter from './appointments.routes';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
 routes.use('/appoitments', appointmentsRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
