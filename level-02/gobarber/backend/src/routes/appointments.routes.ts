607ee951f014d0e6b906ccf6b2d772a54d5221e6
Cintia Zago <cintiazago@ivendas.com.br>
2020-06-27 18:01:42 -0300

    Implementando tratativa de erros globalmente

--- level-02/gobarber/backend/src/routes/appointments.routes.ts
+++ level-02/gobarber/backend/src/routes/appointments.routes.ts
@@ -18,22 +18,18 @@ appoitmentsRouter.get('/', async (request, response) => {
 });
 
 appoitmentsRouter.post('/', async (request, response) => {
-    try {
-        const { provider_id, date } = request.body;
+    const { provider_id, date } = request.body;
 
-        const parsedDate = parseISO(date);
+    const parsedDate = parseISO(date);
 
-        const createAppoint = new CreateAppoitnmentService();
+    const createAppoint = new CreateAppoitnmentService();
 
-        const appointment = await createAppoint.execute({
-            date: parsedDate,
-            provider_id,
-        });
+    const appointment = await createAppoint.execute({
+        date: parsedDate,
+        provider_id,
+    });
 
-        return response.json(appointment);
-    } catch (err) {
-        return response.status(400).json({ error: err.message });
-    }
+    return response.json(appointment);
 });
 
 export default appoitmentsRouter;
--- level-02/gobarber/backend/src/services/CreateUserService.ts
+++ level-02/gobarber/backend/src/services/CreateUserService.ts
@@ -2,6 +2,7 @@ import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
 
 import User from '../models/Users';
+import AppError from '../errors/AppError';
 
 interface Request {
     name: string;
@@ -18,7 +19,7 @@ class CreateUserService {
         });
 
         if (checkUserExists) {
-            throw new Error('Email address already uses.');
+            throw new AppError('Email address already uses.');
         }
 
         const hashedPassword = await hash(password, 8);
